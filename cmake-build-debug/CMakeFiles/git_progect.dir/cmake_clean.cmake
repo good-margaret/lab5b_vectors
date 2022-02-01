5042b53c68722698a9b978d5739d00e7ecc1dbb6
witch_m <good99margaret@gmail.com>
2022-02-01 23:31:22 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/git_progect.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/git_progect.dir/main.c.obj"
+  "git_progect.exe"
+  "git_progect.exe.manifest"
+  "git_progect.pdb"
+  "libgit_progect.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/git_progect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/git_progect.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for git_progect.
+# This may be replaced when dependencies are built.
